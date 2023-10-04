fd5695304279231fc5af34ffefc170d140e2848d
iRinola79 <140052681+Irinola79@users.noreply.github.com>
2023-10-04 15:42:31 +0200

    I finished my calculatrice

---
+++ script.js
@@ -0,0 +1,25 @@
+const buttons = document.querySelectorAll('button');
+const screen = document.getElementById('screen');
+buttons.forEach(button => {
+    button.addEventListener('click', (event) => {
+        
+    });
+});
+buttons.forEach(button => {
+    button.addEventListener('click', (event) => {
+        const value = event.target.value;
+        switch (value) {
+            case '=':
+               
+                screen.textContent = eval(screen.textContent);
+                break;
+            case 'C':
+                
+                screen.textContent = '';
+                break;
+            default:
+             
+                screen.textContent += value;
+        }
+    });
+});
